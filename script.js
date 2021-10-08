71a72a6a416f7f37563babd562c8533312442030
Asel Alimbaeva <asel_19.86@mail.ru>
2021-10-08 21:41:47 +0300

    add lesson 3 and 4

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //---1---Random Number Generation in Javascript: Building a Love Calculator
 
-        //---1---
+//---1---
 let n = Math.random();
-n = n* 6;
+n = n * 6;
 n = Math.floor(n) + 1;
 console.log(n);
 
-        //---2---
+//---2---
 prompt("Your name?");
 prompt("His name");
 
@@ -26,4 +26,22 @@ if (loveScore > 70) {
     alert("Your love score is " + loveScore + "% " + "You love each other like Jack loves Rose.");
 } else {
     alert("Your love score is " + loveScore + "%")
-}    
+}
+
+
+
+
+// ----4 lessons-------
+
+prompt('Как ваше имя? ');
+prompt('Как его или ее зовут? ');
+
+var lovScore = Math.random() * 100;
+lovScore = Math.floor(lovScore) + 1;
+if (lovScore > 70) {
+    alert('Your love scor is ' + lovScore + '%' + 'You love other like kanye loves Konye');
+} else if (lovScore > 30 && lovScore <= 70) {
+    alert('Your love score is ' + lovScore + '%');
+} else if (lovScore <= 30) {
+    alert('Your love score is ' + lovScore + '%' + "You go todether like oil and water.");
+}
